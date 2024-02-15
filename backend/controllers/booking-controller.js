4fc0e2b74bb1560c284e02f77abb49352c4ebf47
Rial <rialparmar007@gmail.com>
2024-02-14 16:29:22 -0800

    booking controller variable

--- backend/controllers/booking-controller.js
+++ backend/controllers/booking-controller.js
@@ -1,19 +1,19 @@
 export const newBooking = async (req, res, next) => {
     const { movie, date, seatNumber, user } = req.body;
 
-    let newBooking;
+    let booking;
 
     try {
-        newBooking = new Bookings({
+        booking = new Bookings({
             movie, date: new Date(`${date}`, seatNumber, user),
         });
-        newBooking = await newBooking.save();
+        booking = await booking.save();
     } catch (err) {
         return console.log(err);
     }
-    if (!newBooking) {
+    if (!booking) {
         return res.status(500).json({message: "Unable to create a booking"})
     }
 
-    return res.status(201).json({booking: newBooking});
+    return res.status(201).json({ booking });
 };
